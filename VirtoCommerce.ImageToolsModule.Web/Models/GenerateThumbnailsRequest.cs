d8c2fc9c264c71cc1e7def4c88404501e90a98f3
dmitry-virtoway <db@virtoway.com>
2016-06-29 12:17:45 +0200

    Updated description of GenerateThumbnailsRequest

--- VirtoCommerce.ImageToolsModule.Web/Models/GenerateThumbnailsRequest.cs
+++ VirtoCommerce.ImageToolsModule.Web/Models/GenerateThumbnailsRequest.cs
@@ -2,17 +2,17 @@
 namespace VirtoCommerce.ImageToolsModule.Web.Models
 {
     /// <summary>
-    /// Request to generate Thumbnails
+    /// Request to generate Thumbnails for image in a platform blob storage.
     /// </summary>
     public class GenerateThumbnailsRequest
     {
         /// <summary>
-        /// Original image url.
+        /// Url of a platform blob storage image.
         /// </summary>
         public string ImageUrl { get; set; }
 
         /// <summary>
-        /// True to replace all existed thumbnails with a new ones.
+        /// True to replace all existed thumbnails with a new ones. False to generate missed only.
         /// </summary>
         public bool IsRegenerateAll { get; set; }
 
