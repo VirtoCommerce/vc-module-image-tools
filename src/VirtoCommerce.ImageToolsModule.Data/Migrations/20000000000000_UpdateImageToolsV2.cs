aaf686c047a819581ccaaa7b45848352a1b08e35
Andrei Kolchanov <akak1977@gmail.com>
2020-09-08 16:03:53 +0500

    VP-4670: Remove dbo schema prefix in migration scripts

--- src/VirtoCommerce.ImageToolsModule.Data/Migrations/20000000000000_UpdateImageToolsV2.cs
+++ src/VirtoCommerce.ImageToolsModule.Data/Migrations/20000000000000_UpdateImageToolsV2.cs
@@ -9,7 +9,7 @@ namespace VirtoCommerce.ImageToolsModule.Data.Migrations
             migrationBuilder.Sql(@"IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '__MigrationHistory'))
                 IF (EXISTS (SELECT * FROM __MigrationHistory WHERE ContextKey = 'VirtoCommerce.ImageToolsModule.Data.Migrations.Configuration'))
                     BEGIN
-	                    INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId],[ProductVersion]) VALUES ('20190410105945_InitialImageTools', '2.2.3-servicing-35854')
+	                    INSERT INTO [__EFMigrationsHistory] ([MigrationId],[ProductVersion]) VALUES ('20190410105945_InitialImageTools', '2.2.3-servicing-35854')
                     END");
         }
 
