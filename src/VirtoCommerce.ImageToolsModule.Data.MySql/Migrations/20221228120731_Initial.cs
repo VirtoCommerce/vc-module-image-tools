bd928d048e2761319e0455a5d7c10982de86e3a9
Oleg Zhuk <zhukoo@gmail.com>
2022-12-28 14:09:58 +0200

    PT-10029: DB Agnostic architecture with SqlServer, MySql and PostgreSQL

---
+++ src/VirtoCommerce.ImageToolsModule.Data.MySql/Migrations/20221228120731_Initial.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.ImageToolsModule.Data.MySql.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ThumbnailOption",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FileSuffix = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ResizeMethod = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Width = table.Column<int>(type: "int", nullable: true),
+                    Height = table.Column<int>(type: "int", nullable: true),
+                    BackgroundColor = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AnchorPosition = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    JpegQuality = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ModifiedBy = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThumbnailOption", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ThumbnailTask",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    WorkPath = table.Column<string>(type: "varchar(2048)", maxLength: 2048, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastRun = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ModifiedBy = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThumbnailTask", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ThumbnailTaskOption",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ThumbnailTaskId = table.Column<string>(type: "varchar(128)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ThumbnailOptionId = table.Column<string>(type: "varchar(128)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThumbnailTaskOption", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ThumbnailTaskOption_ThumbnailOption_ThumbnailOptionId",
+                        column: x => x.ThumbnailOptionId,
+                        principalTable: "ThumbnailOption",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ThumbnailTaskOption_ThumbnailTask_ThumbnailTaskId",
+                        column: x => x.ThumbnailTaskId,
+                        principalTable: "ThumbnailTask",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ThumbnailTaskOption_ThumbnailOptionId",
+                table: "ThumbnailTaskOption",
+                column: "ThumbnailOptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ThumbnailTaskOption_ThumbnailTaskId",
+                table: "ThumbnailTaskOption",
+                column: "ThumbnailTaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ThumbnailTaskOption");
+
+            migrationBuilder.DropTable(
+                name: "ThumbnailOption");
+
+            migrationBuilder.DropTable(
+                name: "ThumbnailTask");
+        }
+    }
+}
---
+++ src/VirtoCommerce.ImageToolsModule.Data.PostgreSql/Migrations/20221228120721_Initial.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.ImageToolsModule.Data.PostgreSql.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ThumbnailOption",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Name = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    FileSuffix = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ResizeMethod = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Width = table.Column<int>(type: "integer", nullable: true),
+                    Height = table.Column<int>(type: "integer", nullable: true),
+                    BackgroundColor = table.Column<string>(type: "text", nullable: true),
+                    AnchorPosition = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true),
+                    JpegQuality = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThumbnailOption", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ThumbnailTask",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Name = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    WorkPath = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    LastRun = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThumbnailTask", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ThumbnailTaskOption",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ThumbnailTaskId = table.Column<string>(type: "character varying(128)", nullable: false),
+                    ThumbnailOptionId = table.Column<string>(type: "character varying(128)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThumbnailTaskOption", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ThumbnailTaskOption_ThumbnailOption_ThumbnailOptionId",
+                        column: x => x.ThumbnailOptionId,
+                        principalTable: "ThumbnailOption",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ThumbnailTaskOption_ThumbnailTask_ThumbnailTaskId",
+                        column: x => x.ThumbnailTaskId,
+                        principalTable: "ThumbnailTask",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ThumbnailTaskOption_ThumbnailOptionId",
+                table: "ThumbnailTaskOption",
+                column: "ThumbnailOptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ThumbnailTaskOption_ThumbnailTaskId",
+                table: "ThumbnailTaskOption",
+                column: "ThumbnailTaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ThumbnailTaskOption");
+
+            migrationBuilder.DropTable(
+                name: "ThumbnailOption");
+
+            migrationBuilder.DropTable(
+                name: "ThumbnailTask");
+        }
+    }
+}
